405cfcc485620f2f5b71755cee843236ebbb840a
Faheem Saleem <faheemusmi@gmail.com>
2023-03-19 16:02:19 +0000

    Initial commit

---
+++ app/src/main/java/com/faheemsaleem/unisoshal/MainActivity.kt
@@ -0,0 +1,44 @@
+package com.faheemsaleem.unisoshal
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val peopleImage = findViewById<ImageView>(R.id.imageView2)
+        val welcomeText = findViewById<TextView>(R.id.textView)
+        val descText = findViewById<TextView>(R.id.textView2)
+        val getStartedButton = findViewById<Button>(R.id.button)
+
+        // set all of the views to alpha 0
+        peopleImage.alpha = 0f
+        welcomeText.alpha = 0f
+        descText.alpha = 0f
+        getStartedButton.alpha = 0f
+
+        peopleImage.translationY = 200f
+        welcomeText.translationY = 200f
+        descText.translationY = 200f
+        getStartedButton.translationY = 20f
+
+        // animate all of the views to alpha 1 and slide up
+        peopleImage.animate().alpha(1f).translationYBy(-200f).setDuration(2000).setInterpolator(android.view.animation.OvershootInterpolator(0.2f)).startDelay = 500
+        welcomeText.animate().alpha(1f).translationYBy(-200f).setDuration(2000).setInterpolator(android.view.animation.OvershootInterpolator(0.2f)).startDelay = 1000
+        descText.animate().alpha(1f).translationYBy(-200f).setDuration(2000).setInterpolator(android.view.animation.OvershootInterpolator(0.2f)).startDelay = 1300
+        getStartedButton.animate().alpha(1f).translationYBy(-20f).setDuration(2000).setInterpolator(android.view.animation.OvershootInterpolator(0.2f)).startDelay = 1500
+
+        // give the button an onClickListener
+        getStartedButton.setOnClickListener {
+            // start the LoginActivity
+            //startActivity(Intent(this, LoginActivity::class.java))
+        }
+
+
+    }
+}
---
+++ app/src/main/java/com/faheemsaleem/unisoshal/SplashActivity.kt
@@ -0,0 +1,47 @@
+package com.faheemsaleem.unisoshal
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityOptionsCompat
+import androidx.core.content.ContextCompat.startActivity
+import kotlinx.coroutines.DelicateCoroutinesApi
+import kotlinx.coroutines.launch
+
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        // hide the status bar
+       // window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
+        // set image alpha to 0
+        findViewById<View>(R.id.imageView).alpha = 0f
+        // animate the image alpha to 1 and scale to 1.1 in 2 seconds and slide up
+        findViewById<View>(R.id.imageView).animate().alpha(1f).scaleX(1.1f).scaleY(1.1f).setDuration(2000).translationY(-20f).start()
+
+        // wait for 3 seconds with a coroutine
+        // then start the MainActivity
+        val handler = Handler()
+        handler.postDelayed({
+            // your code to run after 2 second
+            startMainActivity()
+        }, 2000)
+    }
+
+    private fun startMainActivity() {
+        val intent = Intent(this, MainActivity::class.java)
+        val options = ActivityOptionsCompat.makeSceneTransitionAnimation(
+            this,
+            findViewById(R.id.imageView),
+            "logo"
+        )
+        startActivity(intent, options.toBundle())
+        overridePendingTransition(com.google.android.material.R.anim.abc_shrink_fade_out_from_bottom,
+            com.google.android.material.R.anim.abc_grow_fade_in_from_bottom);
+        //finish()
+    }
+}
